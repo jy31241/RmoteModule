f7ec02351c7946c5e0d2203510e9f4fb6b756825
jy31241 <pjya0321@naver.com>
2018-02-09 16:40:03 +0900

    Module server defined

--- WifiMeshTEST/WifiMeshTEST.ino
+++ WifiMeshTEST/WifiMeshTEST.ino
@@ -1,25 +1,13 @@
 #include <ESP8266WiFi.h>
-
 #include <ESP8266WiFiMesh.h>
 
-
-
 unsigned int request_i = 0;
-
 unsigned int response_i = 0;
-
-
-
 String manageRequest(String request);
-
-
 
 /* Create the mesh node object */
 
 ESP8266WiFiMesh mesh_node = ESP8266WiFiMesh(ESP.getChipId(), manageRequest);
-
-
-
 /**
 
  * Callback for when other nodes send you data
@@ -35,71 +23,33 @@ ESP8266WiFiMesh mesh_node = ESP8266WiFiMesh(ESP.getChipId(), manageRequest);
 String manageRequest(String request)
 
 {
-
 	/* Print out received message */
-
 	Serial.print("received: ");
-
 	Serial.println(request);
 
-
-
 	/* return a string to send back */
-
 	char response[60];
-
 	sprintf(response, "Hello world response #%d from Mesh_Node%d.", response_i++, ESP.getChipId());
-
 	return response;
-
 }
-
-
 
 void setup()
 
 {
-
 	Serial.begin(115200);
 
-	delay(10);
-
-
-
-	Serial.println();
-
-	Serial.println();
-
-	Serial.println("Setting up mesh node...");
-
-
-
 	/* Initialise the mesh node */
-
 	mesh_node.begin();
-
 }
 
-
-
 void loop()
-
 {
-
 	/* Accept any incoming connections */
-
 	mesh_node.acceptRequest();
 
-
-
 	/* Scan for other nodes and send them a message */
-
 	char request[60];
-
 	sprintf(request, "Hello world request #%d from Mesh_Node%d.", request_i++, ESP.getChipId());
-
 	mesh_node.attemptScan(request);
-
 	delay(1000);
-
 }
---
+++ _4X4button_test/_4X4button_test.ino
@@ -0,0 +1,35 @@
+const int numRows= 4;
+const int numCols= 4;
+ 
+int pinRows[numRows] = {2, 3, 4, 5};
+int pinCols[numCols] = {6, 7, 8, 9};
+
+void setup() {
+    Serial.begin(9600);    // init Serial communication
+    
+    // initialize row pins as INPUT_PULLUP
+    for(int i=0; i<numRows; i++) {
+        pinMode(pinRows[i], INPUT_PULLUP);
+    }
+    
+    // initialize column pins as OUTPUT
+    for(int j=0; j<numCols; j++) {
+        pinMode(pinCols[j], OUTPUT);
+        digitalWrite(pinCols[j], HIGH);    // set initial output as HIGH
+    }
+}
+void loop() {
+    for(int j=0; j<numCols; j++) {
+        digitalWrite(pinCols[j], LOW);    // set as LOW to check button press
+        for(int i=0; i<numRows; i++) {
+            if( digitalRead(pinRows[i]) == LOW ) {    // check input. LOW is pressed
+                Serial.print("Pressed row=");
+                Serial.print(i);
+                Serial.print(", column=");
+                Serial.println(j);
+            }
+        }
+        digitalWrite(pinCols[j], HIGH);    // set as default (HIGH)
+    }
+    delay(500);
+}
