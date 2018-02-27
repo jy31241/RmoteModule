327a480cb722b9b69ed3ccf7a886bde199b9877c
Park Jun Yeong <pjya0321@naver.com>
2018-02-27 11:12:16 +0900

    RGB Good

--- Device3AP/Device3AP.ino
+++ Device3AP/Device3AP.ino
@@ -28,6 +28,7 @@ const char indexsource[] =
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt4'\" value=\"GDOWN\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt5'\" value=\"BUP\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt6'\" value=\"BDOWN\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt7'\" value=\"Rainbow\"></INPUT>"
   "</P>"
   "</div>"
   "</FORM>"
@@ -55,6 +56,7 @@ const char rup[] =
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt4'\" value=\"GDOWN\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt5'\" value=\"BUP\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt6'\" value=\"BDOWN\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt7'\" value=\"Rainbow\"></INPUT>"
   "</P>"
   "</div>"
   "</FORM>"
@@ -83,6 +85,7 @@ const char rdown[] =
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt4'\" value=\"GDOWN\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt5'\" value=\"BUP\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt6'\" value=\"BDOWN\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt7'\" value=\"Rainbow\"></INPUT>"
   "</P>"
   "</div>"
   "</FORM>"
@@ -110,6 +113,7 @@ const char gup[] =
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt4'\" value=\"GDOWN\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt5'\" value=\"BUP\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt6'\" value=\"BDOWN\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt7'\" value=\"Rainbow\"></INPUT>"
   "</P>"
   "</div>"
   "</FORM>"
@@ -137,6 +141,7 @@ const char gdown[] =
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt4'\" value=\"GDOWN\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt5'\" value=\"BUP\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt6'\" value=\"BDOWN\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt7'\" value=\"Rainbow\"></INPUT>"
   "</P>"
   "</div>"
   "</FORM>"
@@ -164,6 +169,7 @@ const char bup[] =
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt4'\" value=\"GDOWN\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt5'\" value=\"BUP\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt6'\" value=\"BDOWN\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt7'\" value=\"Rainbow\"></INPUT>"
   "</P>"
   "</div>"
   "</FORM>"
@@ -191,6 +197,35 @@ const char bdown[] =
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt4'\" value=\"GDOWN\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt5'\" value=\"BUP\"></INPUT>"
   "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt6'\" value=\"BDOWN\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt7'\" value=\"Rainbow\"></INPUT>"
+  "</P>"
+  "</div>"
+  "</FORM>"
+  "</body>"
+  "</html>";
+
+const char rainbow[] =
+  "<!DOCTYPE HTML>"
+  "<html>"
+  "<head>"
+  "<meta name = \"viewport\" content = \"width = device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable=0\">"
+  "<titleETRI REMOTE SERVER</title>"
+  "<style>"
+  "\"body { background-color: #808080; font-family: Arial, Helvetica, Sans-Serif; Color: #000000; }\""
+  "</style>"
+  "</head>"
+  "<body>"
+  "<h1>Rainbow</h1>"
+  "<FORM action=\"/\" method=\"post\">"
+  "<div>"
+  "<P>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt1'\" value=\"RUP\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt2'\" value=\"RDOWN\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt3'\" value=\"GUP\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt4'\" value=\"GDOWN\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt5'\" value=\"BUP\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt6'\" value=\"BDOWN\"></INPUT>"
+  "<INPUT style=\"background:#1AAB8A; color:#fff;  border:none;  position:relative;  height:30px;  font-size:1.6em;  padding:0 2em; cursor:pointer;  transition:800ms ease all;  outline:none;border-width:2px;border-style:solid;\" type=\"button\" class=\"button\" onclick=\"location.href='/bt7'\" value=\"Rainbow\"></INPUT>"
   "</P>"
   "</div>"
   "</FORM>"
@@ -202,59 +237,115 @@ int R = 0;
 int G = 0;
 int B = 0;
 
-rgbLed.writeRGB(R, G, B);
-
 void indexpg() {
   server.send(200, "text/html", indexsource);
 }
 
 void button1() {
   server.send(200, "text/html", rup);
-  R += 32;
+  R += 75;
   rgbLed.writeRGB(R, G, B);
 }
 
 void button2() {
   server.send(200, "text/html", rdown);
-  R -= 32;
+  R -= 75;
   rgbLed.writeRGB(R, G, B);
 }
 
 void button3() {
   server.send(200, "text/html", gup);
-  G += 32;
+  G += 75;
   rgbLed.writeRGB(R, G, B);
 }
 
 void button4() {
   server.send(200, "text/html", gdown);
-  G -= 32;
+  G -= 75;
   rgbLed.writeRGB(R, G, B);
 }
 
 void button5() {
   server.send(200, "text/html", bup);
-  B += 32;
+  B += 75;
   rgbLed.writeRGB(R, G, B);
 }
 
 void button6() {
   server.send(200, "text/html", bdown);
-  B -= 32;
+  B -= 75;
   rgbLed.writeRGB(R, G, B);
+}
+
+int delayMs = 1000;
+void button7() {
+  server.send(200, "text/html", rainbow);
+  rgbLed.writeRGB(255, 0, 0);
+  delay(delayMs);
+
+  //Set the RGBLED to show GREEN only
+  rgbLed.writeRGB(0, 255, 0);
+  delay(delayMs);
+
+  //Set the RGBLED to show BLUE only
+  rgbLed.writeRGB(0, 0, 255);
+  delay(delayMs);
+
+  //Set the RGBLED to show YELLOW (RED & GREEN) only
+  rgbLed.writeRGB(255, 255, 0);
+  delay(delayMs);
+
+  //Set the RGBLED to show ORANGE (RED & partial GREEN) only
+  rgbLed.writeRGB(255, 128, 0);
+  delay(delayMs);
+
+  //Set the RGBLED to show PURPLE (RED & BLUE) only
+  rgbLed.writeRGB(255, 0, 255);
+  delay(delayMs);
+
+  //Set the RGBLED to show PINK (RED & partial BLUE) only
+  rgbLed.writeRGB(255, 0, 128);
+  delay(delayMs);
+
+  //Set the RGBLED to show a random color
+  rgbLed.writeRandom();
+  delay(delayMs);
+
+  //Set the pins individually if needed
+  rgbLed.writeRed(255);
+  rgbLed.writeGreen(255);
+  rgbLed.writeBlue(255);
+  delay(delayMs);
+
+  //The above code does the same thing as...
+  rgbLed.writeRGB(255, 255, 255);
+  delay(delayMs);
+
+  //Show the color wheel
+  Serial.println("Showing RGB Color Wheel...");
+  Serial.println("------------------------------");
+  //Use a 25ms delay between each color in the wheel
+  rgbLed.writeColorWheel(25);
+
+  //Turn off the RGBLED
+  rgbLed.turnOff();
+  delay(delayMs);
 }
 
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
+
   String ledType = (rgbLed.commonType == 0) ? "COMMON_CATHODE" : "COMMON_ANODE";
+  rgbLed.writeRGB(R, G, B);
 
   Serial.print("Configuring access point...");
   WiFi.softAP(ssid, password);
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
+
   server.on("/", indexpg);
   server.on("/bt1", button1);
   server.on("/bt2", button2);
@@ -262,6 +353,7 @@ void setup() {
   server.on("/bt4", button4);
   server.on("/bt5", button5);
   server.on("/bt6", button6);
+  server.on("/bt7", button7);
   server.begin();
   Serial.println("-----------ETRI REMOTE Server Started--------");
 }
